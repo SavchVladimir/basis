b749e63c7e71390fe8dbce03cffcfab834053605
Vladimir Savchenko <yzbest@yandex.ru>
2016-01-03 17:31:07 +0300

    сборка, очистка старого проекта. Проверить wiredep & useref

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,11 @@ var gulp 		= require("gulp"),
 	filter 		= require('gulp-filter'),
 	imagemin 	= require('gulp-imagemin'),
 	useref      = require('gulp-useref'),		//html
-	gulpif 		= require('gulp-if')
+	gulpif 		= require('gulp-if'),
+	wiredep 	= require('wiredep').stream,
+	jade 		= require('gulp-jade'),
+	del 		= require('del'),
+	size 		= require('gulp-size')
 	;
 
 //----------------------server--------------------------//
@@ -26,6 +30,8 @@ gulp.task('server', function(){
 //----------------------watch--------------------------//
 gulp.task('watch', function() {
 	gulp.watch('app/scss/**/*.scss',['sass']);
+	gulp.watch('app/templates/pages/**/*.jade',['jade']);
+	gulp.watch('bower.json', ['wiredep']);
 	gulp.watch([
 		'app/*.html',
 		'app/js/**/*.js',
@@ -34,46 +40,7 @@ gulp.task('watch', function() {
 });
 //>>>-------------default------------------------------//
 gulp.task('default', ['server', 'watch']);
-//-------------------CSS----------------------------//
-//собираем в dist минимизируем в prod
-gulp.task('css', function () {
-  return gulp.src('app/css/**/*.css')
-    .pipe(concatCss("fullstyle.css")) 
-    .pipe(gulp.dest('dist/'))
-    .pipe(minifyCss())
-    .pipe(rename("fullstyle-min.css"))
-    .pipe(gulp.dest('prod/'));
-});
-//-------------------JS----------------------------//
-//собираем в dist минимизируем в prod
-gulp.task('js', function () {
-  return gulp.src('app/js/**/*.js')
-    .pipe(gulp.dest('dist/'))
-    .pipe(uglify())
-    .pipe(rename("fulljs-min.js"))
-    .pipe(gulp.dest('prod/'));
-});
-//-------------------HTML----------------------------//
-//link в html и копируем в dist и prod
-  // gulp.task('useref', function () {
-  //   return gulp.src('app/*.html')
-  //     .pipe(useref())
-  //     .pipe(gulp.dest('dist'))
-  //     .pipe(gulp.dest('prod'));
-  // });
-
-// Переносим HTML, CSS, JS в папку dist 
-// gulp.task('useref', function () {
-//   var assets = useref.assets();
-//   return gulp.src('dist/*.html')
-//     .pipe(assets)
-//     .pipe(gulpif('*.js', uglify()))
-//     .pipe(gulpif('*.css', minifyCss({compatibility: 'ie8'})))
-//     .pipe(assets.restore())
-//     .pipe(useref())
-//     .pipe(gulp.dest('prod'));
-// });
-
+//-------------------SASS----------------------------//
 gulp.task('sass', function() {
   return gulp.src('app/scss/*.scss')
     .pipe(sass({
@@ -84,7 +51,42 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('app/css'))
     .pipe(reload({stream: true})); 
 });
-
+//-------------------JADE----------------------------//
+gulp.task('jade', function() {
+  return gulp.src('app/templates/pages/*.jade')
+    .pipe(jade({pretty:true}))
+    .pipe(gulp.dest('app/'))
+    .pipe(reload({stream: true}));
+});
+//-------------------CSS----------------------------//
+//собираем в dist
+gulp.task('css', function () {
+  return gulp.src('app/css/**/*.css')
+    //.pipe(concatCss("fullstyle.css")) 
+    .pipe(gulp.dest('dist'));
+});
+//-------------------JS----------------------------//
+//собираем в dist
+gulp.task('js', function () {
+  return gulp.src('app/js/**/*.js')
+    .pipe(gulp.dest('dist'));
+});
+//-------------------JS----------------------------//
+//собираем в dist
+gulp.task('html', function () {
+  return gulp.src('app/**/*.html')
+    .pipe(gulp.dest('dist'));
+});
+//-------------------HTML----------------------------//
+//link в html и копируем из dist в prod
+// Переносим HTML, CSS, JS в папку prod
+gulp.task('useref', function () {
+    return gulp.src('dist/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('prod'));
+});
 //-------------------FONTS----------------------------//
 //Переносим шрифты
 gulp.task('fonts', function() {
@@ -114,3 +116,30 @@ gulp.task('extras', function () {
 	.pipe(gulp.dest('dist'))
 	.pipe(gulp.dest('prod'));
 });
+//---------------BOWER---------------------------------//
+gulp.task('wiredep', function () {
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app/'))
+  });
+//---------------DEL---------------------------------//
+gulp.task('del', function () {
+   del(['dist','prod']);
+});
+//>>>------------------собираем------------------------------//
+// Сборка
+//css и  js копирует в папку dist
+//html минимизирует и объединяет в папку prod
+//['images', 'fonts', 'extras'] - копируют в dist и prod
+gulp.task('prod', ['css', 'js', 'html', 'images', 'fonts', 'extras', 'useref'], function () {
+  return gulp.src('prod/**/*').pipe(size({title: 'build'}));
+});
+
+//(только после очистки и компиляции Jade и sass и подключении стилей и js)
+//у [jade', 'sass', 'wiredep'] действие происходит в папке app
+gulp.task('build', ['del', 'jade', 'sass'], function () {
+  gulp.start('prod');
+});
+//------------------------------------------------//
